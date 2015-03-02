5bfb10f84b300ab00eb6691bff3c2690d88a9168
Letitia Popescu <letitiap@gmail.com>
2015-03-01 16:17:31 -0800

    Initial push

---
+++ src/deer.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python
+
+import time
+import os
+import commands
+import RPi.GPIO as GPIO
+import json
+import sys
+import smtplib
+
+from email.encoders import encode_base64
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+PARAMS = {
+    u"tz": "America/Los_Angeles",
+    u"time_sec": 15,
+    u"resolution": "960x540",
+    u"preview": 1,
+    u"savedir": u"/home/pi/deer/video",
+    u"gpio_port": 21,
+    u"open_box_limit_sec": 40,
+    u"mail_movie": 0,
+    u"send_mail": 0,
+    }
+
+def getLanIp(lan):
+    (stat, out) = commands.getstatusoutput('ifconfig %s | grep "inet addr:" | cut -d : -f 2 | cut -d " " -f1' % lan)
+    if stat or not out:
+        return None
+    else:
+        return out
+
+def sendMail(params, subject, message, attach=None):
+    """Mail sending utility function."""
+    lan_addr = getLanIp('wlan0')
+    if not lan_addr:
+       # lan_addr = getLanIp('eth0')
+       if not lan_addr:
+           print "Not connected to the network  - skipping mail"
+           return
+
+    from_addr = '%s@%s' % (params["smtp_user"], params["smtp_domain"])
+    to_addr = params["alert_email"]
+    message += "\n -- Sent from http://%s/" % lan_addr
+    if attach is None:
+        msg = MIMEText(message, 'plain')
+    else:
+        msg = MIMEMultipart('alternative')
+        msg.attach(MIMEText(message, 'plain'))
+        for (mime_type, mime_subtype, name, bytes) in attach:
+            payload = MIMEBase(mime_type, mime_subtype)
+            payload.set_payload(bytes)
+            encode_base64(payload)
+            payload.add_header('Content-Disposition',
+                               'attachment; filename="%s"' % name)
+            msg.attach(payload)
+
+    msg['Subject'] = subject
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    print "Sending to: ", params['smtp_server'], "/", params['smtp_port']
+    smtp = smtplib.SMTP_SSL(params['smtp_server'],
+                            params['smtp_port'])
+    smtp.login(from_addr, params['smtp_pass'])
+    smtp.sendmail(from_addr, [to_addr], msg.as_string())
+    smtp.quit()
+    print " === Mail sent to %s ==" % to_addr
+
+def triggerOpenBox(p):
+    print "Box is left open"
+    if p['send_mail']:
+        try:
+            sendMail(p, "Mail Box Alert!", "Your Mailbox was left open - we disabled video recording. Please close it to restart video recording")
+        except:
+            print " -- Error sending email -- "
+
+def triggerRecord(p):
+    # if not p.get("smtp_pass", ""):
+    #    p["smtp_pass"]  = getpass.getpass("Please enter your smtp user password :")
+
+    n = p["gpio_port"]
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(n, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    off_time = 0
+    box_left_open = False
+    while True:
+        state = GPIO.input(n)
+        if not state:
+            if not box_left_open:
+                recordVideo(p)
+            if time.time() - off_time > p["open_box_limit_sec"]: 
+                if not box_left_open: 
+                    triggerOpenBox(p)
+                box_left_open = True
+        else: 
+            off_time = time.time()
+            box_left_open = False
+        time.sleep(0.1)
+         
+
+def recordVideo(p):
+    t = time.localtime()
+    cur_day = time.strftime("%Y_%m_%d", t)
+    cur_time = time.strftime("%H_%M_%S", t)
+    savedir = os.path.join(p["savedir"], cur_day)
+    try:
+        os.mkdir(savedir)
+    except:
+        pass
+
+    sz = p['resolution'].split('x')
+    width = int(sz[0])
+    height = int(sz[1])
+    preview = ""
+    if p["preview"]:
+        preview = "-p 0,150,%s,%s" % (width, height)
+    
+    h264_file = "%s/%s.h264" % (savedir, cur_time)
+    movie_file = "%s/%s.mp4" % (savedir, cur_time)
+    image_file = "%s/%s.jpeg" % (savedir, cur_time)
+   
+    # record video from the camera as in instructed by 
+    # our parameters 
+    cmd = "raspivid -w %s -h %s -t %s -o %s %s" % (
+        width, height, p["time_sec"]*1000,
+        h264_file, preview)    
+
+    print cmd
+    (stat, out) = commands.getstatusoutput(cmd)
+    print "%s / %s" % (stat, out)
+    if stat != 0:
+        return
+   
+    # make the recorded file an mp4
+    cmd = "MP4Box -fps 30 -add %s %s" % (h264_file, movie_file)
+    print cmd
+    (stat, out) = commands.getstatusoutput(cmd)
+    print "%s / %s" % (stat, out)
+    if stat != 0:
+        return
+
+    # get a frame as an image file
+    cmd = "ffmpeg -i %s -ss 0.5 -vframes 1 -f image2 %s" % (movie_file, image_file)
+    print cmd
+    (stat, out) = commands.getstatusoutput(cmd)
+    print "%s / %s" % (stat, out)
+    if stat != 0:
+        return
+
+    os.unlink(h264_file)
+    if not p["send_mail"]:
+        return
+    try:
+        content = None
+        if p["mail_movie"]:
+            with open(movie_file) as f:
+                content = [('video', 'mpeg', movie_file, f.read())]
+        else:
+            with open(image_file) as f:
+                content =  [('image', 'jpeg', image_file, f.read())]
+
+        sendMail(p, "Mail Box Alert!", "Your Mailbox was Opened!", content)
+    except:
+        print " -- Cannot send mail -- "
+
+def loadParams():
+    config = json.loads(open(sys.argv[1]).read()) 
+    global PARAMS
+    PARAMS.update(config)
+    os.environ['TZ'] = config.get('tz', 'America/Los_Angeles')
+    time.tzset()
+
+if __name__ == "__main__":
+    loadParams()
+    # except: pass
+    triggerRecord(PARAMS)
---
+++ www/deer_site.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python
+
+import web
+import time
+import glob
+import os
+import sys
+import json
+import commands
+
+config_file = '/home/pi/deer/cfg/deer.cfg'
+
+urls = (
+    '/videolist', 'videolist',
+    '/config', 'configurator',
+    '/restart', 'restarter',
+)
+url_prefix = '/video/'
+videodir = '/home/pi/deer/video/'
+t_globals = dict(
+)
+
+render = web.template.render('/home/pi/deer/www/templates/', cache=False,  globals=t_globals)
+
+def getVideoFiles(uri_prefix, video_dir):
+    thumbs = glob.glob(videodir + "/2*/*.jpeg")
+    thumbs.sort()
+    thumbs.reverse()
+    videos = []
+    for t in thumbs:
+        jpeg_fn = os.path.basename(os.path.dirname(t)) + "/" + os.path.basename(t)
+        mp4_fn = jpeg_fn[:-4] + "mp4"
+        tspec = time.strptime(jpeg_fn, "%Y_%m_%d/%H_%M_%S.jpeg")
+        videos.append( (time.asctime(tspec), uri_prefix + jpeg_fn, uri_prefix + mp4_fn, jpeg_fn[:-4]) )
+    return videos
+
+class videolist:
+    def GET(self):
+        videos = getVideoFiles(url_prefix, videodir)
+        videolist = '\n'.join(unicode(render.videocell(t[0], t[1], t[2], t[3])) for t in videos)
+        return render.videolist(renderer=render, videolist=videolist)
+
+    def POST(self):
+        delroot =  GetStringParam('delroot', None)
+        if delroot:
+            pattern = videodir + "/" + delroot + "*"
+            files = glob.glob(pattern)
+            for f in files:
+                print " Deleting file :", f
+                try:
+                    os.unlink(f)
+                except:
+                    pass
+        return self.GET()
+
+def GetStringParam(name, default=None):
+    param = web.input()
+    if param.has_key(name):
+        return param.get(name)
+    return default
+
+def GetIntParam(name, default=0):
+    param = web.input()
+    if not param.has_key(name):
+        return default
+    try:
+        return int(param.get(name))
+    except ValueError, e:
+        return default
+
+def GetWlanEssid():
+    (stat, out) = commands.getstatusoutput('iwconfig 2>/dev/null | grep ESSID | cut -d \'"\' -f 2')
+    print "Essid: %s / %s" % (stat, out)
+    if stat or not out:
+        return None
+    else:
+        return out
+
+def GetWlanIp():
+    (stat, out) = commands.getstatusoutput('ifconfig wlan0 | grep "inet addr:" | cut -d : -f 2 | cut -d " " -f1')
+    print "wlanip: %s / %s" % (stat, out)
+    if stat or not out:
+        return None
+    else:
+        return out
+
+class restarter:
+    def GET(self):
+        with open(config_file) as f:
+            config = json.loads(f.read())
+        config['wlan_network'] = GetWlanEssid()
+        return render.configure(renderer=render, config=config, wlan_ip=GetWlanIp())
+
+    def POST(self):
+        (stat, out) = commands.getstatusoutput('reboot')
+        print "Reboot: %s/ %s" % (stat, out)
+        return render.restart()
+
+class configurator:
+    def GET(self):
+        with open(config_file) as f:
+            config = json.loads(f.read())
+        config['wlan_network'] = GetWlanEssid()
+        return render.configure(renderer=render, config=config, wlan_ip=GetWlanIp())
+
+    def POST(self):
+        error = []
+        
+        with open(config_file) as f:
+            config = json.loads(f.read())
+
+        config['resolution'] =  GetStringParam('size_select', config['resolution'])
+        time_sec = GetIntParam('timesec', config['time_sec'])
+        if time_sec < 5:
+            error.append('Recording time to short')
+        else:
+            config['time_sec'] = time_sec
+
+        config['open_timespec'] = GetIntParam('open_timesec', config['open_box_limit_sec'])
+        if (config['open_timespec'] < config['time_sec']):
+            error.append('Open time too short')
+            config['open_timespec'] = 2 * config['time_sec']
+            
+        send_mail = GetIntParam('send_mail', 0)
+        if send_mail: config['send_mail'] = 0
+        else: config['send_mail'] = 1
+
+        smtp_email = GetStringParam('smtp_email', config['smtp_user'] + '@' + config['smtp_domain'])
+        comp = smtp_email.split('@')
+        if len(comp) == 2:
+            config['smtp_user'] = comp[0]
+            config['smtp_domain'] = comp[1]
+        elif config['send_mail']:
+            error.append('Invalid sender email address')
+
+        smtp_server = GetStringParam('smtp_server', '%s:%s' % (config['smtp_server'], config['smtp_port']))
+        comp = smtp_server.split(':')
+        if len(comp) == 2:
+            config['smtp_server'] = comp[0]
+            try:
+                config['smtp_port'] = int(comp[1])
+            except:
+                error.append('Invalid smtp server')
+        else:
+            config['smtp_server'] = smtp_server
+
+        smtp_pass = GetStringParam('smtp_pass', '')
+        if smtp_pass and smtp_pass != 'pass_set':
+            config['smtp_pass'] = smtp_pass
+
+        alert_email = GetStringParam('alert_email', config['alert_email'])
+        comp = smtp_email.split('@')
+        if len(comp) == 2:
+            config['alert_email'] = alert_email
+        elif config['send_mail']:
+            error.append('Invalid alert email address')
+
+        mail_movie = GetStringParam('mail_movie', 'jpg')
+        if mail_movie == 'mpg':
+            config['mail_movie'] = 1
+        else:
+            config['mail_movie'] = 0
+
+        try:
+            with open(config_file + '_', 'w') as f:            
+                f.write(json.dumps(config))
+            os.rename(config_file + '_', config_file)
+        except:
+            error.append('Error saving the configuration')
+
+        (stat, out) = commands.getstatusoutput('restart deer')
+        print "Restart - %s / %s" % (stat, out)
+        if stat:
+            error.append('Error restarting the monitoring service.')
+
+        wlan_net = GetStringParam('wlan_network', '')
+        wlan_pass = GetStringParam('wlan_pass', '')
+        restart = False
+        if wlan_net and wlan_pass and wlan_pass != 'pass_set':
+            with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'a') as wpa:
+                wpa.write("\nnetwork={\n  ssid=\"%s\"\n  psk=\"%s\"\n}\n" % (wlan_net, wlan_pass));
+                restart = True
+
+        config['wlan_network'] = GetWlanEssid()
+
+        return render.configure(renderer=render, config=config, wlan_ip=GetWlanIp(), error='\n'.join(error), restart=restart)
+        
+
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.internalerror = web.debugerror
+    app.run()
